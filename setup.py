ea195aa76cd4098f60531126e4238885c4b74459
Sean Lee <freshdried@gmail.com>
2014-04-29 01:27:49 -0400

    fixed versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='PEA_connect',
-    version='1.0',
+    version='1.1',
     description="Facilitates retrieval/manipulation of Exeter Connect Data",
     url="https://github.com/Exeter/PEA_connect",
     download_url="https://github.com/Exeter/PEA_connect",
